51fb1e2b667a279c6375deb9cb1fbfeca82224c2
unknown <asthaagarwal203@gmail.com>
2022-03-30 13:03:26 +0530

    [Astha] Added: UC8 Create Datatable and add values

--- ProductReviewManagementLINQ/Program.cs
+++ ProductReviewManagementLINQ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,13 @@ namespace ProductReviewManagementLINQ
             foreach (var product in result)
             {
                 Console.WriteLine("Product id is "+product.productId+" User id is "+product.userId+" product rating is "+product.rating);
+            }
+        }
+        public static void DisplayTable(DataTable product)
+        {
+            foreach (DataRow row in product.Rows)
+            {
+                Console.WriteLine("Product id is "+row["ProductID"]+" Rating is "+row["Rating"]);
             }
         }
         static void Main(string[] args)
@@ -59,6 +67,31 @@ namespace ProductReviewManagementLINQ
             //uc6
             var result5 = products.Skip(5);
             Display(result5.ToList());
+
+            //uc8
+            DataTable productDataTable = new DataTable();
+            productDataTable.Columns.Add("ProductID", typeof(int));
+            productDataTable.Columns.Add("UserID",typeof(int));
+            productDataTable.Columns.Add("Rating",typeof (int));
+            productDataTable.Columns.Add("Review");
+            productDataTable.Columns.Add("IsLike", typeof(bool));
+            productDataTable.Rows.Add(1, 12, 5, "good", "true");
+            productDataTable.Rows.Add(2, 15, 2, "good", "true");
+            productDataTable.Rows.Add(3, 56, 4, "good", "true");
+            productDataTable.Rows.Add(4, 34, 1, "bad", "false");
+            productDataTable.Rows.Add(5, 12, 5, "good", "true");
+            productDataTable.Rows.Add(6, 15, 2, "good", "true");
+            productDataTable.Rows.Add(7, 56, 4, "good", "true");
+            productDataTable.Rows.Add(8, 34, 1, "bad", "false");
+            productDataTable.Rows.Add(9, 12, 5, "good", "true");
+            productDataTable.Rows.Add(10, 15, 2, "good", "true");
+            productDataTable.Rows.Add(11, 56, 4, "good", "true");
+            productDataTable.Rows.Add(12, 34, 1, "bad", "false");
+            productDataTable.Rows.Add(13, 12, 5, "good", "true");
+            productDataTable.Rows.Add(14, 15, 2, "good", "true");
+            productDataTable.Rows.Add(15, 56, 4, "good", "true");
+            productDataTable.Rows.Add(16, 34, 1, "bad", "false");
+            DisplayTable(productDataTable);
         }
     }
 
