ac0c68464cf2b6950cd6d540f0cd92f070b8a299
unknown <asthaagarwal203@gmail.com>
2022-03-30 11:30:21 +0530

    [Astha] Added: Retrieve top three records from linq

--- ProductReviewManagementLINQ/Program.cs
+++ ProductReviewManagementLINQ/Program.cs
@@ -17,20 +17,26 @@ namespace ProductReviewManagementLINQ
                 new Products() { productId = 3, userId = 2, rating = 5, review = "bad", isLike = false },
                 new Products() { productId = 1, userId = 3, rating = 6, review = "good", isLike = true },
                 new Products() { productId = 5, userId = 1, rating = 6, review = "good", isLike = true },
-                new Products() { productId = 3, userId = 1, rating = 4, review = "good", isLike = true },
+                new Products() { productId = 3, userId = 1, rating = 10, review = "good", isLike = true },
                 new Products() { productId = 7, userId = 2, rating = 5, review = "bad", isLike = false },
                 new Products() { productId = 8, userId = 3, rating = 6, review = "good", isLike = true },
-                new Products() { productId = 11, userId = 1, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 11, userId = 1, rating = 12, review = "good", isLike = true },
                 new Products() { productId = 10, userId = 1, rating = 4, review = "bad", isLike = false },
                 new Products() { productId = 11, userId = 2, rating = 5, review = "good", isLike = true },
                 new Products() { productId = 10, userId = 3, rating = 6, review = "good", isLike = true },
                 new Products() { productId = 12, userId = 3, rating = 6, review = "good", isLike = true },
                 new Products() { productId = 1, userId = 1, rating = 6, review = "good", isLike = true },
-                new Products() { productId = 3, userId = 1, rating = 4, review = "bad", isLike = false },
+                new Products() { productId = 3, userId = 1, rating = 1, review = "bad", isLike = false },
                 new Products() { productId = 11, userId = 2, rating = 5, review = "good", isLike = true },
-                new Products() { productId = 5, userId = 3, rating = 6, review = "good", isLike = true }
+                new Products() { productId = 5, userId = 3, rating = 13, review = "good", isLike = true }
             };
+            //uc2
+            var result = products.OrderByDescending(x=>x.rating).Take(3);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.rating);
+            }
         }
     }
 
-    }
+  }
