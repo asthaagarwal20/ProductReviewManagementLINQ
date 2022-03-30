13abbd8ffcbd45a403e89f0682fc361278ea51e7
unknown <asthaagarwal203@gmail.com>
2022-03-30 10:41:14 +0530

    [Astha] Added: UC1_Create Product Review Class and make list

---
+++ ProductReviewManagementLINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> products = new List<Products>()
+            {
+                new Products() { productId = 1, userId = 1, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 2, userId = 1, rating = 4, review = "good", isLike = true },
+                new Products() { productId = 3, userId = 2, rating = 5, review = "bad", isLike = false },
+                new Products() { productId = 1, userId = 3, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 5, userId = 1, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 3, userId = 1, rating = 4, review = "good", isLike = true },
+                new Products() { productId = 7, userId = 2, rating = 5, review = "bad", isLike = false },
+                new Products() { productId = 8, userId = 3, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 11, userId = 1, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 10, userId = 1, rating = 4, review = "bad", isLike = false },
+                new Products() { productId = 11, userId = 2, rating = 5, review = "good", isLike = true },
+                new Products() { productId = 10, userId = 3, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 12, userId = 3, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 1, userId = 1, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 3, userId = 1, rating = 4, review = "bad", isLike = false },
+                new Products() { productId = 11, userId = 2, rating = 5, review = "good", isLike = true },
+                new Products() { productId = 5, userId = 3, rating = 6, review = "good", isLike = true }
+            };
+        }
+    }
+
+    }
